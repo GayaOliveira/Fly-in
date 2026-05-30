ef60ed3e36e26d75d4e4b0c56dd0df4ad90bc5ca
gmarinho <supergaya@yahoo.com>
2026-05-29 23:57:16 -0300

    feat: add main for tests

---
+++ main.py
@@ -0,0 +1,11 @@
+from file_loader import FileLoader
+from parser import Parser
+
+if __name__ == "__main__":
+
+    loader = FileLoader("03_priority_puzzle.txt")
+    raw = loader.get_config()
+
+    parser = Parser(raw)
+    data = parser.parse()
+    # print(data)
