4c2caa27b9f57d7cb536d72dee835f38d0bb5a4f
gmarinho <supergaya@yahoo.com>
2026-06-01 21:32:26 -0300

    feat: add main for tests

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from errors import ParseError
 from file_loader import FileLoader
 from parser import Parser
 
 if __name__ == "__main__":
+    try:
+        loader = FileLoader("03_priority_puzzle.txt")
+        raw = loader.get_config()
 
-    loader = FileLoader("03_priority_puzzle.txt")
-    raw = loader.get_config()
+        parser = Parser(raw)
+        data = parser.parse()
 
-    parser = Parser(raw)
-    data = parser.parse()
-    print(data)
+        print("nb_drones =>", data["nb_drones"])
+
+        for element in data["graph_elements"]:
+            print(element)
+
+    except ParseError as error:
+        print(f"\033[31mError: {error}\033[m")
