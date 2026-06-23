c76a8aaef99eee18cae4c86cee0db120326facfd
gmarinho <supergaya@yahoo.com>
2026-06-22 21:12:43 -0300

    feat: add connection entity

---
+++ entity/connection.py
@@ -0,0 +1,26 @@
+from schema import ConnectionSchema
+from hub import Hub
+
+
+class Connection:
+    def __init__(
+        self,
+        hub_pair: list[Hub],
+        max_link_capacity: int
+    ) -> None:
+        self.hub_pair = hub_pair
+        self.max_link_capacity = max_link_capacity
+
+    @classmethod
+    def from_schema(cls, schema: ConnectionSchema, hubs: list[Hub]):
+        hub_map = {hub.name: hub for hub in hubs}
+
+        hub_pair = [
+            hub_map[hub_schema.name]
+            for hub_schema in schema.hub_pair
+        ]
+
+        return cls(
+            hub_pair=hub_pair,
+            max_link_capacity=schema.max_link_capacity
+        )
