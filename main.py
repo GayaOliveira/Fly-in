0652d6ba75c592c4fc638bf329b9a77063561751
gmarinho <supergaya@yahoo.com>
2026-06-23 20:44:15 -0300

    feat: add main for tests

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from errors import ParseError
 from file_loader import FileLoader
 from parser import Parser
 from gui import GrafoApp
+from entity import Map, Drone
+from dijkstra import Dijkstra
 
 if __name__ == "__main__":
     try:
-        loader = FileLoader("03_ultimate_challenge.txt")
+        loader = FileLoader("03_priority_puzzle.txt")
         raw = loader.get_config()
 
         parser = Parser(raw)
         data = parser.parse()
+
+        map_obj = Map.from_parsed(data)
+
+        Dijkstra(map_obj).move_drone(Drone((0, 0)))
 
         # print("nb_drones =>", data["nb_drones"])
 
